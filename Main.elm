d72c5ed0cc2d417bee13f548c4cb805619a346ee
Tedd Arcuri <tarcuri88@gmail.com>
2016-08-06 17:14:31 -0600

    updating Elm app to official guide example

--- Main.elm
+++ Main.elm
@@ -1,7 +1,42 @@
 import Html exposing (..)
+import Html.App as Html
+import Html.Events exposing (onClick)
 
-say str =
-    str ++ " !"
 
 main =
-  text (say "Hello World")
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
+
+-- MODEL
+
+type alias Model = Int
+
+model : Model
+model =
+  0
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+    Decrement ->
+      model - 1
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div[]
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
