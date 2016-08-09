e86bd5ecc951a95fa864ca2d734b58ebce39c3e9
teddarcuri <tedd.arcuri@gmail.com>
2016-08-08 22:27:33 -0600

    Adding sweet input app. WOAHHH

--- Main.elm
+++ Main.elm
@@ -1,7 +1,7 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.App as Html
-import Html.Events exposing (onClick)
-
+import Html.Events exposing (..)
 
 main =
   Html.beginnerProgram
@@ -12,31 +12,71 @@ main =
 
 -- MODEL
 
-type alias Model = Int
+type alias Model =
+  { message : String }
 
 model : Model
 model =
-  0
+  { message = "cool"}
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg
+  = ChangeMessage String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model + 1
-    Decrement ->
-      model - 1
-
+    ChangeMessage newMessage ->
+      { model | message = newMessage }
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  div[]
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
+  div []
+    [ mainHeader model
+    , body model
     ]
+
+mainHeader model =
+  header [ headerStyle ]
+    [ ul []
+      [ li [ headerLiStyle ]
+        [text "Home"]
+      , li [ headerLiStyle ]
+        [text model.message]
+      , li [ headerLiStyle ]
+        [text "Learnin shit!"]
+      , li [ headerLiStyle ]
+        [text "Sweet Link!"]
+      ]
+    ]
+
+body model =
+  div [ bodyStyles ]
+    [ h1 [] [ text ("Elm is " ++ model.message) ]
+    , input [ placeholder model.message, onInput ChangeMessage ] []
+    , p [] [ text model.message ]
+    ]
+
+-- Styles
+
+headerStyle =
+  style
+    [ ("width", "100%")
+    , ("height", "50px")
+    , ("position", "fixed")
+    , ("top", "0")
+    , ("left", "0")
+    , ("background", "#444")
+    ]
+
+headerLiStyle =
+  style
+    [ ("display", "inline-block")
+    , ("padding", "0px 10px")
+    ]
+
+bodyStyles =
+  style [ ("margin-top", "100px")]
